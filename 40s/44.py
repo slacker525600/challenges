a1143b574d333ed33d9ffaa6549600953dcf058c
slacker525600 <ecker.chris.r@gmail.com>
2013-11-24 13:42:50 -0600

    solved 44

--- 40s/44.py
+++ 40s/44.py
@@ -1,15 +1,37 @@
-import math
 anPentagonal = []
 nOn = 0
-while bUnFinished:
-  nNew = n*(3*n -1)/2
-  for nLower in anPentagonal:
-    nDif = math.abs(nNew - nLower)
-    nSum = math.abs(nNew + nLower)
-    if nDif in anPentagonal:
-      #want to check if sum is in ... but ... really just need to check if 
-      nSum * 2 # ... uh ... == n (n*3 - 1) == n^2 *3 - n
+nPentOn = 0
+nMinDifference = 1000000
+
+nPentDif = 1
+nNew = 1
+
+while nMinDifference > nPentDif:
+  #tests
+  for nLower in anPentagonal[:nOn]:
+    #print(anPentagonal)
+    nDif = abs(nPentOn - nLower)
+    nSum = abs(nPentOn + nLower)
+    while anPentagonal[-1] < nSum:
+      #get up to the sum, to check if it is pentagonal
+      anPentagonal.append(nNew)
+      nPentDif += 3 
+      nNew += nPentDif
+
+    if nDif in anPentagonal and nSum in anPentagonal:
+      print('reached here, ', nDif)
+      nMinDifference = min(nDif, nMinDifference)
+  #always add one
   anPentagonal.append(nNew)
- 
+  nPentDif += 3 
+  nNew += nPentDif
+
+  nPentOn = anPentagonal[nOn]
+  nOn += 1  
+  #print(nOn, nPentOn, nPentDif)
+  if nOn > 1000000:
+    break
+
+print(nMinDifference)
   
 
